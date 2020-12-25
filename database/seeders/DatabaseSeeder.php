3032f9d8e58b4704210d8f2adb5f764a4ff5f82d
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2020-12-25 23:31:00 +0800

    [Add] Add Database Seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Password;
+use App\Models\Vault;
+use App\Models\VaultPassword;
+use App\Models\VaultUser;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 
@@ -14,6 +18,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(1)->create();
+        $user = User::factory(1)->create();
+        $vault = Vault::factory()->create();
+        $password = Password::factory()->create([
+            'type' => "password",
+            'content' => json_encode([])
+        ]);
+        VaultPassword::factory()->create([
+            'vault' => $vault['id'],
+            'password' => $password['id']
+        ]);
+
+        VaultUser::factory()->create([
+           'user' => $user[0]->id,
+           'vault' => $vault['id'],
+           'permission' => "ADMIN"
+        ]);
     }
 }
