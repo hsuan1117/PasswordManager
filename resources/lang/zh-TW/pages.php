1fcb0121e2557bddb03cb99463319476e3814ba4
Hsuan <34859514+hsuan1117@users.noreply.github.com>
2021-06-23 11:08:40 +0800

    [Update] Tidy up Welcome page

---
+++ resources/lang/zh-TW/pages.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dashboard' => [
+        'welcome' => '歡迎使用 Cotpaer 密碼管理器',
+        'description' => '點擊上方的「儲存庫」即可開始使用密碼管理器'
+    ],
+    'vault' => [
+        'title' => '儲存庫列表',
+        'add' => '新增儲存庫',
+        'view' => '查看儲存庫',
+        'my' => '我的儲存庫'
+    ]
+];
